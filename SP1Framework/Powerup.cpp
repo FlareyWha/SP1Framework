9d52673e61a56fe1bae5d5a05f87412e7cebb2aa
qnise <joeligma317@gmail.com>
2020-08-26 15:17:57 +0800

    added stuff to Powerup

--- SP1Framework/Powerup.cpp
+++ SP1Framework/Powerup.cpp
@@ -1,1 +1,39 @@
 #include "Powerup.h"
+
+Powerup::Powerup()
+{
+}
+
+Powerup::~Powerup()
+{
+}
+
+int Powerup::cheaperFood()
+{
+	return 0;
+}
+
+int Powerup::cheaperRent()
+{
+	return 0;
+}
+
+int Powerup::playerShoes()
+{
+	return 0;
+}
+
+int Powerup::slowerCustomers()
+{
+	return 0;
+}
+
+int Powerup::richCustomers()
+{
+	return 0;
+}
+
+int Powerup::thriftyCustomers()
+{
+	return 0;
+}
--- SP1Framework/Powerup.h
+++ SP1Framework/Powerup.h
@@ -1,6 +1,15 @@
 #pragma once
 class Powerup
 {
+public:
+	Powerup();
+	~Powerup();
 
+	int cheaperFood(); //cheaper food
+	int cheaperRent(); //cheaper rent
+	int playerShoes(); //faster player
+	int slowerCustomers(); //slower customers
+	int richCustomers(); //more money per customer
+	int thriftyCustomers(); //less chance of buying >1 item
 };
 
