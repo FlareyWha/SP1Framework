0ef9ebf72a90866eb59180fdcdbdd4863f3ec93d
Meowington914 <69460649+Meowington914@users.noreply.github.com>
2020-08-26 10:29:28 +0800

    Customer box change colour i think ???

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -42,11 +42,13 @@ Son* cPtr[2] = { nullptr, nullptr };
 
 Player p;
 
-Box* boxPtr[7];
-Position* boxPosPtr[7];
+Box* boxPtr[7] = { nullptr, nullptr, nullptr, nullptr, nullptr , nullptr, nullptr };
+Position* boxPosPtr[7] = { nullptr, nullptr, nullptr, nullptr, nullptr , nullptr, nullptr };
 WORD BoxColour;
 Map map;
 int framesPassed;
+
+WORD CustomerBoxColour[6];
 
 // Console object
 int g_ConsoleX = 80;
@@ -1372,7 +1374,8 @@ void renderBoxes()
     for (int i = 0; i < 6; i++)
     {
         if (boxPtr[i + 1] != nullptr) {
-            g_Console.writeToBuffer(boxPosPtr[i + 1]->getX(), boxPosPtr[i + 1]->getY(), ' ', BoxColour);
+            
+            g_Console.writeToBuffer(boxPosPtr[i + 1]->getX(), boxPosPtr[i + 1]->getY(), ' ', CustomerBoxColour[i]);
         }
         
     }
@@ -1447,6 +1450,8 @@ void renderCustomer() // fix later yes ues
                     boxPtr[i + 1] = new Box;
                     boxPosPtr[i + 1] = new Position;
                 }
+
+                CustomerBoxColour[i] = 0x77;
                 
                 if (boxPosPtr[i + 1] != nullptr) {
                     boxPosPtr[i + 1]->setX((customerPtr[i]->getX()) + 1);
@@ -1457,7 +1462,7 @@ void renderCustomer() // fix later yes ues
                 if ((timer[i] >= 30.95) && (timer[i] <= 31.05))
                 {
                     bool bComplain = false;
-                    //only did first 2 shelves and its kinda inefficient
+                    
 
                     for (int j = 0; j < 3; j++) {
 
@@ -1471,7 +1476,18 @@ void renderCustomer() // fix later yes ues
 
                                     p.AddDayEarnings(customerPtr[i]->getQuantity()); //for adding amount earned daily// can change it if need be
 
-
+                                    switch (j) 
+                                    {
+                                        case 0:
+                                            CustomerBoxColour[i] = 0x50;
+                                            break;
+                                        case 1:
+                                            CustomerBoxColour[i] = 0x10;
+                                            break;
+                                        case 3:
+                                            CustomerBoxColour[i] = 0xB0;
+                                            break;
+                                    }
                                 }
 
                                 else if (sPtr[j]->getAmount() < customerPtr[i]->getQuantity()) {
@@ -1496,6 +1512,19 @@ void renderCustomer() // fix later yes ues
 
 
                                     p.AddDayEarnings(customerPtr[i]->getQuantity()); //for adding amount earned daily// can change it if need be
+
+                                    switch (j)
+                                    {
+                                    case 3:
+                                        CustomerBoxColour[i] = 0xE0;
+                                        break;
+                                    case 4:
+                                        CustomerBoxColour[i] = 0xA0;
+                                        break;
+                                    case 5:
+                                        CustomerBoxColour[i] = 0x90;
+                                        break;
+                                    }
 
 
                                 }
@@ -1526,6 +1555,7 @@ void renderCustomer() // fix later yes ues
                     delete boxPosPtr[i + 1];
                     boxPosPtr[i + 1] = nullptr;
 
+                    CustomerBoxColour[i] = 0x77;
 
                     timer[i] = -1;
                     travelling[i] = false;
