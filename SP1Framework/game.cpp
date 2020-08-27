347f8f68f8f4410a163cbe7f327fef018fca9e82
FlareyWha <69341849+FlareyWha@users.noreply.github.com>
2020-08-27 16:23:30 +0800

    a fix for infinite dissatisfaction (hopefully)

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -1965,7 +1965,9 @@ void renderCustomer()
 
                                 else if (sPtr[j]->getAmount() < customerPtr[i]->getQuantity()) {
                                     p.increaseUnsatisfiedCustomers();
-
+                                    customerPtr[i]->setEndPoint(79, 15);
+                                    avoiding[i] = 5;
+                                    travelling[i] = false;
                                 }
 
                             }
